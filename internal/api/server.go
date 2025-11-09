dfcda993891ad002bb54eba6c482214021962482
srivastavya02 <srivastavya24@gmail.com>
2025-11-09 18:30:24 +0530

    feat: integrate cluster heartbeat with gRPC server startup

--- internal/api/server.go
+++ internal/api/server.go
@@ -4,10 +4,12 @@ import (
 	"context"
 	"fmt"
 	"net"
+	"net/http"
 
 	"google.golang.org/grpc"
 
 	"github.com/jerkeyray/mimori/internal/api/kv"
+	"github.com/jerkeyray/mimori/internal/cluster"
 	"github.com/jerkeyray/mimori/internal/storage"
 )
 
@@ -53,10 +55,26 @@ func (s *Server) Health(ctx context.Context, _ *kv.HealthRequest) (*kv.HealthRes
 
 // server launcher
 func ListenAndServe(addr string, store storage.KV) error {
+	// start the cluster heartbeat routine
+	peers := []string{":4000", ":4001", ":4002"} // temporary static config
+	cluster := cluster.New(addr, peers)
+	cluster.Start()
+	defer cluster.Stop()
+
 	lis, err := net.Listen("tcp", addr)
 	if err != nil {
 		return fmt.Errorf("listen: %w", err)
 	}
+
+	// HTTP health endpoint
+	go func() {
+		http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+			_, _ = w.Write([]byte("ok"))
+		})
+		_ = http.ListenAndServe(addr, nil)
+	}()
+
 	grpcServer := grpc.NewServer()
 	// route calls to KV service to this implementation
 	kv.RegisterKVServer(grpcServer, NewServer(store))
